2ae62ded5fdacf8ef5c88a90cf846f44a42a96fe
JoeFleck05 <joefleck05@gmail.com>
2017-04-30 21:29:06 -0400

    failed gwt helloserver

---
+++ de.jfleck.gwt.helloserver/src/de/jfleck/gwt/helloserver/server/MyUserServiceImpl.java
@@ -0,0 +1,41 @@
+package de.jfleck.gwt.helloserver.server;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import de.jfleck.gwt.helloserver.client.model.MyUser;
+import de.jfleck.gwt.helloserver.client.service.MyUserService;
+
+public class MyUserServiceImpl extends RemoteServiceServlet implements MyUserService {
+	private static final long serialVersionUID = 1L;
+
+    private List<MyUser> userList = new ArrayList<MyUser>();
+
+    public MyUserServiceImpl() {
+            MyUser user = new MyUser();
+            user.setId("1");
+            user.setUsername("Peter");
+            user.setNumberOfHits("15");
+            userList.add(user);
+
+            user = new MyUser();
+            user.setId("2");
+            user.setUsername("Hanz");
+            user.setNumberOfHits("25");
+            userList.add(user);
+    }
+
+    public MyUser getUser(String id) {
+
+            for (Object object : userList) {
+                    if (((MyUser) object).getId().equals(id))
+                            return ((MyUser) object);
+            }
+            return null;
+    }
+
+    public List<MyUser> getUserList() {
+            return userList;
+    }
+
+}
